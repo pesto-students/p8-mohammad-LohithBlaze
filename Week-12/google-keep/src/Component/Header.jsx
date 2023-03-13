ff036e1337c2535d36534f72a29654815cc5c54c
blaze <lohith@stonovation.com>
2023-03-13 08:13:44 +0530

    week10-assignment

---
+++ Week-12/google-keep/src/Component/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <img src="https://cdn-icons-png.flaticon.com/512/2965/2965358.png" alt="logo" width="70" height="50" />
+                <h1>Google Keep</h1>
+            </div>
+        </>
+    );
+};
+
+export default Header;
